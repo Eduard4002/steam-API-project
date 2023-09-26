3ae3ba5e7bbb4f1cb72a9605475143af7510691c
Your Name <jonathan.thj15@gmail.com>
2023-09-26 09:59:47 +0200

    Header and icons in index 	modified:   index.html 	modified:   src/assets/components/header.jsx 	modified:   src/assets/css/header.css 	new file:   src/assets/img/heart.svg 	new file:   src/assets/img/person.svg

--- src/assets/components/header.jsx
+++ src/assets/components/header.jsx
@@ -1,16 +1,30 @@
 import "../css/header.css";
+import Logo from '../img/logo.png'
+import Person from '../img/person.svg'
+import Heart from '../img/heart.svg'
 
 function Header() {
 
   return (
     <>
-        <nav className="navBar">
-            <div className="leftNav">
-                <img src="" alt=""/>
-                <h1>Steam API</h1>
-            </div>
-            
-        </nav>
+      <nav className="navBar">
+        <div className="leftNav">
+          <img src={Logo} alt="Logo" />
+          <h1>Game API</h1>
+        </div>
+        <div className="middleNav">
+          <input
+            type="search"
+            name="SearchInput"
+            id="searchInput"
+            placeholder="Search"
+          />
+        </div>
+        <div className="rightNav">
+          <span className="profileIcon"><img src={Person} alt="Profile" /></span>
+          <span className="favoIcon"><img src={Heart} alt="Favorites" /></span>
+        </div>
+      </nav>
     </>
   );
 }
