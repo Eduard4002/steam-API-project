023de74803b401c3cd2725e2ecb36f3080916d3f
Your Name <jonathan.thj15@gmail.com>
2023-09-26 14:06:48 +0200

    App 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-
-
-function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -58,6 +52,6 @@ function App() {
       </h5>
     </>
   );
-}
+
 
 export default App
