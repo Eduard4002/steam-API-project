3ed590e906fc95bec10453f5f775b4e1c90a536b
Mizfra <vipkotenok939@gmail.com>
2023-09-26 14:01:35 +0200

    yes

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Jonte är Bäst!!!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import Header from './assets/components/header.jsx'
 import Footer from './assets/components/footer.jsx'
 import './index.css'
+import Default from './assets/components/Default.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
+    <Default/>
     <App />
     <Footer />
   </React.StrictMode>,
