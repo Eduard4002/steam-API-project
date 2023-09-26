5271d021644f061ffd4dadf974989386ea60807d
Your Name <jonathan.thj15@gmail.com>
2023-09-26 09:24:21 +0200

    Header 	modified:   src/assets/components/header.jsx 	modified:   src/assets/css/header.css

---
+++ src/assets/components/header.jsx
@@ -0,0 +1,18 @@
+import "../css/header.css";
+
+function Header() {
+
+  return (
+    <>
+        <nav className="navBar">
+            <div className="leftNav">
+                <img src="" alt=""/>
+                <h1>Steam API</h1>
+            </div>
+            
+        </nav>
+    </>
+  );
+}
+
+export default Header;
