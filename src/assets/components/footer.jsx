0958989b8f4c17702f1c240932714c636c3f5e09
Your Name <jonathan.thj15@gmail.com>
2023-09-26 13:47:17 +0200

    Adding starting script in footer 	modified:   src/assets/components/footer.jsx

---
+++ src/assets/components/footer.jsx
@@ -0,0 +1,13 @@
+import "../css/footer.css";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <div>Footer, GoodBye!</div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
