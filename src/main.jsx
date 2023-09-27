bfaa9a5f2e4243a589ea342a5648fc0f4965d672
Your Name <jonathan.thj15@gmail.com>
2023-09-27 12:10:32 +0200

    Add react router example

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,60 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { Games } from "./routes/Games.jsx";
 import Header from "./assets/components/header.jsx";
 import Footer from "./assets/components/footer.jsx";
 import "./index.css";
 import Default from "./assets/components/Default.jsx";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  useParams,
+  Link,
+} from "react-router-dom";
+
+const Wrapper = () => (
+  <>
+    <Header />
+    <Default />
+    <Outlet />
+    <Footer />
+  </>
+);
+const Test = () => {
+  let { gameId } = useParams();
+
+  return (
+    <>
+      <h1>Game: {gameId}</h1>
+      <Link to={"/"}>hehe</Link>
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    element: <Wrapper />,
+    children: [
+      {
+        path: "",
+        element: <App />,
+      },
+      {
+        path: "game/:gameId",
+        element: <Test />,
+      },
+    ],
+  },
+  {
+    path: "game",
+    element: <Games />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Header />
-    <Default />
-    <App />
-    <Footer />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/routes/Games.jsx
@@ -0,0 +1,7 @@
+export const Games = () => {
+  return (
+    <div>
+      <h1>The games</h1>
+    </div>
+  );
+};
