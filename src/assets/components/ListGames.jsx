e7016580c21ad08fb73f96b1c868ea6e65f0405c
Eduard4002 <mihiceduard@gmail.com>
2023-09-27 11:47:44 +0200

    Displaying index on list games

--- src/assets/components/ListGames.jsx
+++ src/assets/components/ListGames.jsx
@@ -2,71 +2,79 @@ import React, { useEffect, useState } from "react";
 
 function ListGames() {
   const englishCharacterRegex = /^[A-Za-z0-9\s]+$/;
-
-  let [data, setData] = useState([]);
+  const [data, setData] = useState([]);
   const [displayedGames, setDisplayedGames] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0); // Start at 0
+
   const gamesPerPage = 10;
   const maxGames = 100;
+
+  // Function to filter and limit games
+  const filterAndLimitGames = (games) => {
+    // Check if the game has a name (non-empty) and if the name contains only English characters
+    const filteredData = games.filter((item) => {
+      return item.name && englishCharacterRegex.test(item.name);
+    });
+
+    // Shuffle the filtered games randomly
+    const shuffledGames = [...filteredData].sort(() => Math.random() - 0.5);
+
+    // Limit the shuffled games to the variable maxGames
+    const limitedGames = shuffledGames.slice(0, maxGames);
+
+    return limitedGames;
+  };
 
   //Retrieve the information from the server
   useEffect(() => {
     fetch("http://localhost:3000/api")
       .then((response) => response.json())
-      .then((json) => setData(json.applist.apps))
+      .then((json) => {
+        const games = json.applist.apps;
+        setData(games);
 
+        // Call the filtering and limiting function and set displayedGames
+        const limitedGames = filterAndLimitGames(games);
+        setDisplayedGames(limitedGames);
+      })
       .catch((error) => console.error(error));
-    // Check if the game has a name (non-empty) and if the name contains only English characters
-    data = data.filter((item) => {
-      return item.name && englishCharacterRegex.test(item.name);
-    });
-    // Shuffle the filtered games randomly
-    const shuffledGames = [...data].sort(() => Math.random() - 0.5);
-
-    // Limit the shuffled games to the variable maxGames
-    const limitedGames = shuffledGames.slice(0, maxGames);
-
-    // Set the displayed games in the state
-    setDisplayedGames(limitedGames);
   }, []);
 
   // Function to load the next 10 games
   const loadNextGames = () => {
-    if (currentIndex + gamesPerPage < displayedGames.length) {
-      setCurrentIndex((prevIndex) => prevIndex + gamesPerPage);
-    }
+    setCurrentIndex((prevIndex) => prevIndex + 1);
   };
 
   // Function to load the previous 10 games if needed
   const loadPreviousGames = () => {
-    if (currentIndex > 0) {
-      setCurrentIndex((prevIndex) => prevIndex - gamesPerPage);
-    }
+    setCurrentIndex((prevIndex) => prevIndex - 1);
   };
+  console.log(displayedGames);
   return (
     <div>
       <button onClick={loadPreviousGames} disabled={currentIndex === 0}>
         <p key="prev">Previous</p>
       </button>
+      <p>{currentIndex + 1}</p> {/* Add 1 to display the 1-based index */}
       <button
         onClick={loadNextGames}
-        disabled={currentIndex + gamesPerPage >= displayedGames.length}
+        disabled={(currentIndex + 1) * gamesPerPage >= maxGames}
       >
         <p key="next">Next</p>
       </button>
       {displayedGames
-        .slice(currentIndex, currentIndex + gamesPerPage)
+        .slice(
+          currentIndex * gamesPerPage,
+          currentIndex * gamesPerPage + gamesPerPage
+        )
         .map((game, index) => (
-          <>
-            <div className="container">
-              <p key={game.appid + 1}>{game.appid}</p>
-
-              <p key={index}>{game.name}</p>
-            </div>
-          </>
+          <div className="container" key={game.appid + index}>
+            <p>{game.appid}</p>
+            <p>{game.name}</p>
+          </div>
         ))}
     </div>
   );
 }
-//return <>{data && data.map((item) => console.log(item.name))}</>;
+
 export default ListGames;
