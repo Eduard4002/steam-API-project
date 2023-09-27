4cfee10f8a527ea86574f5c547166b3381644424
Eduard4002 <mihiceduard@gmail.com>
2023-09-27 09:59:36 +0200

    Added filter for only english games

--- src/assets/components/ListGames.jsx
+++ src/assets/components/ListGames.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 function ListGames() {
-  const [data, setData] = useState([]);
-  /*
-  var req = new XMLHttpRequest();
-  req.open("GET", "http://localhost:3000/getnews", false);
-  req.send(null);
-  console.log(JSON.parse(req.responseText));*/
+  const englishCharacterRegex = /^[A-Za-z0-9\s]+$/;
 
+  let [data, setData] = useState([]);
+
+  //Retrieve the information from the server
   useEffect(() => {
     fetch("http://localhost:3000/api")
       .then((response) => response.json())
-      .then((json) => setData(json.applist))
+      .then((json) => setData(json.applist.apps))
 
       .catch((error) => console.error(error));
   }, []);
-  return <>{data.apps && data.apps.map((item) => console.log(item.name))}</>;
-}
 
+  // Check if the game has a name (non-empty) and if the name contains only English characters
+  data = data.filter((item) => {
+    return item.name && englishCharacterRegex.test(item.name);
+  });
+  return <>{data && <p>{data.length}</p>}</>;
+}
+//return <>{data && data.map((item) => console.log(item.name))}</>;
 export default ListGames;
