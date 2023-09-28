302809f0e5865d7cd747aea35bdc00bce9702200
4CEspX <simon@jonassonmail.se>
2023-09-28 11:27:22 +0200

    Games page gooder

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {
   useParams,
 } from "react-router-dom";
 import App from "./App.jsx";
-import Default from "./assets/components/Default.jsx";
+import ListGames from "./assets/components/ListGames.jsx";
 import Footer from "./assets/components/footer.jsx";
 import Header from "./assets/components/header.jsx";
 import "./index.css";
