29cfe64f40dbf186644b19cb27e55bd0115ce708
Your Name <jonathan.thj15@gmail.com>
2023-09-28 11:26:40 +0200

    Games fixed 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -15,7 +15,7 @@ import "./index.css";
 import LogIn from "./logInPage.jsx";
 import Profile from "./profile.jsx";
 import SignUp from "./signUpPage.jsx";
-import ListGames from "";
+import ListGames from "./assets/components/ListGames.jsx";
 
 const Wrapper = () => (
   <>
