e9df138ad574b43ea16499efff1f4d23d2b15a9a
Your Name <jonathan.thj15@gmail.com>
2023-10-02 12:06:23 +0200

    Favorite 	modified:   src/assets/css/favorites.css 	modified:   src/favorites.jsx

--- src/favorites.jsx
+++ src/favorites.jsx
@@ -1,7 +1,8 @@
 import "./assets/css/favorites.css";
-// import imagePlaceholder from "./assets/img/imgPlaceholder.jpg";
+import imagePlaceholder from "./assets/img/imgPlaceholder.jpg";
 import StuckMenu from "./assets/components/stuckMenu"; // Import your Slideshow component
 import ToggleVisibility from "./assets/components/ToggleVisibility";
+import { Link } from "react-router-dom";
 
 function Favorites() {
   return (
@@ -10,7 +11,31 @@ function Favorites() {
         <StuckMenu /> {/* Use the Slideshow component */}
       </ToggleVisibility>
       <div className="favoritesDiv">
-        
+        <h1>Your Favorites</h1>
+        <Link to={""}>
+          <div className="favoriteCard">
+            <img src={imagePlaceholder} alt="Game thumbnail" />
+            <div className="favoriteText">
+              <h3>Game Title</h3>
+              <p>Hejsan på digsan</p>
+            </div>
+            <div className="favoriteSettings">
+              <span className="material-symbols-outlined">grade</span>
+            </div>
+          </div>
+        </Link>
+        <Link to={""}>
+          <div className="favoriteCard">
+            <img src={imagePlaceholder} alt="Game thumbnail" />
+            <div className="favoriteText">
+              <h3>Game Title</h3>
+              <p>Hejsan på digsan</p>
+            </div>
+            <div className="favoriteSettings">
+              <span className="material-symbols-outlined">grade</span>
+            </div>
+          </div>
+        </Link>
       </div>
     </>
   );
