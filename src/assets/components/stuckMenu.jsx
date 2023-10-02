7076515a0e63678c057b262094a98ba633db15bd
Eduard4002 <mihiceduard@gmail.com>
2023-10-02 10:30:57 +0200

    Data Array file added

--- src/assets/components/stuckMenu.jsx
+++ src/assets/components/stuckMenu.jsx
@@ -1,17 +1,28 @@
-import "../css/sidebar.css"
-
-
+import "../css/sidebar.css";
+import { Link } from "react-router-dom";
+import DataArray from "../../DataArray";
 export default function stuckMenu() {
+  const data = DataArray();
+
+  if (data.length === 0) return <h1>LOADING</h1>;
+  const randomGameIndex = Math.floor(Math.random() * data.length);
   return (
-    <div className="default-container" >
-
+    <div className="default-container">
       <ul>
-        <li><a> Main Page</a></li>
-        <li><a>Games</a></li>
-        <li><a>Something</a></li>
-        <li><a>Random</a></li>
-        </ul>
-    </div>
-  );  
-}
+        <Link to={"/"} className="link">
+          Main Page
+        </Link>
+        <Link to={"/games"} className="link">
+          Games
+        </Link>
 
+        <Link to={"/"} className="link">
+          Something
+        </Link>
+        <Link to={"/game/" + data[randomGameIndex].appid} className="link">
+          Random
+        </Link>
+      </ul>
+    </div>
+  );
+}
