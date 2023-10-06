737fc6571f7bfc3748082ccfbfd4d7e3687cbfe0
Gurrnos <gurrnos01@gmail.com>
2023-10-06 09:29:44 +0200

    database stuff early

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
 import express from "express";
 var app = express();
 import request from "request";
+import sqlite3 from "sqlite3";
+sqlite3.verbose()
+const db = new sqlite3.Database('./db/db.sqlite')
+
+db.serialize(() => {
+  db.run('CREATE TABLE accounts (data TEXT)')
+  const stmt = db.prepare('INSERT INTO accounts VALUES (?)')
+  VALUES
+    (email, password, username, id, favorites, )
+
+  for (let i = 0; i < 10; i++) {
+    stmt.run(`Ipsum ${i}`)
+  }
+
+  stmt.finalize()
+
+  db.each('SELECT rowid AS id, info FROM lorem', (err, row) => {
+    console.log(`${row.id}: ${row.info}`)
+  })
+})
+
+db.close()
 
 app.set("port", 3000);
 
@@ -48,3 +70,4 @@ app.listen(app.get("port"), function () {
       "; press Ctrl-C to terminate."
   );
 });
+
