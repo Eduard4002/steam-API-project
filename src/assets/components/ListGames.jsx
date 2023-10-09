a289ecb14b85ccc2a57abd996f8cfcbce43eb335
Mizfra <vipkotenok939@gmail.com>
2023-10-09 12:39:42 +0200

    add button sidemenu ttill ListGames

--- src/assets/components/ListGames.jsx
+++ src/assets/components/ListGames.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "../css/listGames.css";
 import "../css/single.css";
+import ToggleVisibility from "./ToggleVisibility";
+import StuckMenu from "./stuckMenu"; // Import your Slideshow component
 
 function ListGames({ dataToDisplay, maxGames = 20, gamesPerPage = 5 }) {
   const [extraData, setExtraData] = useState([]);
@@ -94,6 +96,10 @@ function ListGames({ dataToDisplay, maxGames = 20, gamesPerPage = 5 }) {
   console.log(fav);
   return (
     <>
+    <ToggleVisibility>
+        <StuckMenu /> {/* Use the Slideshow component */}
+      </ToggleVisibility>
+
       <div>
         <div className="mainDiv">
           <div className="contFlex">
