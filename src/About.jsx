d3a9ce197b47dc269bf5f0a70da2176527d1ad1c
Mizfra <vipkotenok939@gmail.com>
2023-10-10 09:29:53 +0200

    grid about

--- src/About.jsx
+++ src/About.jsx
@@ -11,83 +11,91 @@ function About(){
 
 <h1>About Us</h1>
         
+
+        
 <div className="main">
 
 
-<div className="InfoGeneralCont">
+<div className="aboutGrid">
+   
+    
+    
+    
+    <div className="lables">
+    
+    
+        <div className="InfoCont">
+            <img src="" alt="" />
+    
+            <div className="infoTextCont">
+                <h3 className="names"></h3>
+                <p className="infoText"></p>
+                <button className="gitBttn"> GitHub</button>
+            </div>
+    
+        </div>
+    
+    
+        <div className="InfoCont">
+            <img src="" alt="" />
+    
+            <div className="infoTextCont">
+                <h3 className="names"></h3>
+                <p className="infoText"></p>
+                <button className="gitBttn">GitHub</button>
+            </div>
+    
+        </div>
+    
+    
+        <div className="InfoCont">
+            <img src="" alt="" />
+    
+            <div className="infoTextCont">
+                <h3 className="names"></h3>
+                <p className="infoText"></p>
+                <button className="gitBttn">GitHub</button>
+            </div>
+    
+        </div>
+    
+        <div className="InfoCont">
+            <img src="" alt="" />
+    
+            <div className="infoTextCont">
+                <h3 className="names"></h3>
+                <p className="infoText"></p>
+                <button className="gitBttn">GitHub</button>
+            </div>
+    
+        </div>
+    
+    
+    
+    
+        <div className="InfoCont">
+            <img src="" alt="" />
+    
+            <div className="infoTextCont">
+                <h3 className="names"></h3>
+                <p className="infoText"></p>
+                <button className="gitBttn">GitHub</button>
+            </div>
+    
+        </div>
+    </div>
 
+
+    <div className="InfoGeneralCont">
+    
     <div className="infoGeneral">
-        <h3 className="Textinfo"></h3>
+        <h3 className="Textinfo">Game API</h3>
         <p className="AboutUs"> </p>
 
     </div>
 
 </div>
 
-
-
-<div className="lables"> 
-
-
-    <div className="InfoCont">
-        <img src="" alt="" />
-
-        <div className="infoTextCont">
-            <h3 className="names"></h3>
-            <p className="infoText"></p>
-            <button className="gitBttn"> GitHub</button>
-        </div>
-
-    </div>
-
-
-    <div className="InfoCont">
-        <img src="" alt="" />
-
-        <div className="infoTextCont">
-            <h3 className="names"></h3>
-            <p className="infoText"></p>
-            <button className="gitBttn">GitHub</button>
-        </div>
-
-    </div>
-
-
-    <div className="InfoCont">
-        <img src="" alt="" />
-
-        <div className="infoTextCont">
-            <h3 className="names"></h3>
-            <p className="infoText"></p>
-            <button className="gitBttn">GitHub</button>
-        </div>
-
-    </div>
-
-    <div className="InfoCont">
-        <img src="" alt="" />
-
-        <div className="infoTextCont">
-            <h3 className="names"></h3>
-            <p className="infoText"></p>
-            <button className="gitBttn">GitHub</button>
-        </div>
-
-    </div>
-
-
-
-
-    <div className="InfoCont">
-        <img src="" alt="" />
-
-        <div className="infoTextCont">
-            <h3 className="names"></h3>
-            <p className="infoText"></p>
-            <button className="gitBttn">GitHub</button>
-        </div>
-
-    </div>
 </div>
 
 </div>
