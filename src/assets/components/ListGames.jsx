c12d190906c7c5464638534f0d4132dfaabc711a
Jonte88 <jonathan.thj15@gmail.com>
2023-10-10 10:00:31 +0200

    Adding some logo stuff and working on listgames

--- src/assets/components/ListGames.jsx
+++ src/assets/components/ListGames.jsx
@@ -104,60 +104,63 @@ function ListGames({ dataToDisplay, maxGames = 20, gamesPerPage = 5 }) {
             {extraData.map((game, index) => (
               <>
                 <Link to={"/game/id/" + game.steam_appid} key={game.steam_appid}>
-                  <div className="container" key={game.appid + index}>
-                    {game && (
-                      <img
-                        src={
-                          game.header_image || "src/assets/img/placeholder.webp"
-                        }
-                        key={game.header_image}
-                        className="image"
-                      ></img>
-                    )}
-                    <div className="nameAndDescDiv">
-                      <div className="textDiv">
-                        <h2 key={game.name}>{game.name}</h2>
+                  <div className="gameDiv" key={game.appid + index}> {/*-------------------- Old Classname = container ------------*/}
+                    <div className="gameGrid">
+                      <div className="gameImageGridItem gameGridItem">
+                        {game && (
+                          <img
+                            src={
+                              game.header_image || "src/assets/img/placeholder.webp"
+                            }
+                            key={game.header_image}
+                            className="image"
+                          ></img>
+                        )}
                       </div>
-
-                      {game?.short_description && (
-                        <div
-                          className="description"
-                          key={game?.short_description}
-                        >
-                          {/*Does short description exists*/}
-                          {game?.short_description != "" || (
-                            <p>
-                              There does not appear to be a short description
-                              for this game
-                            </p>
-                          )}
-                          {/*Is short description too large to fit inside of the container?*/}
-                          {game?.short_description.length <
-                          descriptionMaxLength ? (
-                            <p key={game.short_description}>
-                              {game.short_description}
-                            </p>
-                          ) : (
-                            /*Short description is too large to fit inside of the container*/
-                            <p key={game.short_description}>
-                              {game?.short_description.slice(
-                                0,
-                                descriptionMaxLength
-                              ) + "..."}
-                            </p>
-                          )}
+                      <div className="gameTextGridItem gameGridItem">
+                        <div className="textDiv">
+                          <h2 key={game.name}>{game.name}</h2>
                         </div>
-                      )}
-                    </div>
-                    <div className="buttonsDiv">
-                      <div
-                        className={`star ${
-                          fav.find((item) => item.appid === game.steam_appid)
-                            ? "active"
-                            : "inactive"
-                        }`}
-                        key={game.steam_appid + 2}
-                      ></div>
+                        {game?.short_description && (
+                          <div
+                            className="description"
+                            key={game?.short_description}
+                          >
+                            {/*Does short description exists*/}
+                            {game?.short_description != "" || (
+                              <p>
+                                There does not appear to be a short description
+                                for this game
+                              </p>
+                            )}
+                            {/*Is short description too large to fit inside of the container?*/}
+                            {game?.short_description.length <
+                            descriptionMaxLength ? (
+                              <p key={game.short_description}>
+                                {game.short_description}
+                              </p>
+                            ) : (
+                              /*Short description is too large to fit inside of the container*/
+                              <p key={game.short_description}>
+                                {game?.short_description.slice(
+                                  0,
+                                  descriptionMaxLength
+                                ) + "..."}
+                              </p>
+                            )}
+                          </div>
+                        )}
+                      </div>
+                      <div className="buttonsDiv">
+                        <div
+                          className={`star ${
+                            fav.find((item) => item.appid === game.steam_appid)
+                              ? "active"
+                              : "inactive"
+                          }`}
+                          key={game.steam_appid + 2}
+                        ></div>
+                      </div>
                     </div>
                   </div>
                 </Link>
