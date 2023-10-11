f8ab1608210ebb0cbc26603696200ab9534d8c77
Mizfra <vipkotenok939@gmail.com>
2023-10-11 14:21:28 +0200

    sidebar gone yes

--- src/assets/components/ToggleVisibility.jsx
+++ src/assets/components/ToggleVisibility.jsx
@@ -1,23 +1,42 @@
 
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 export default function ToggleVisibility({ children }) {
 
+
+
+let MenuIsOpen = false;
   
   const [show, setShow] = useState();
   console.log (window.location.pathname);
 
-  
+useEffect(()=>{
+  window.addEventListener("click", function (e) {
+    if (show) {
+      if (e.target.id != "me") {
+       
+
+      setShow(false);
+      }
+    }
+  });
+
+}
+
+)
+  console.log(show)
  
   function toggleShow() {
     setShow(!show);
+    
   }
   var button = show ? "<" : ">";
 
   return (
     <div className={`component-container ${show ? "show-sidebar" : ""}`}> 
       {show && children}
-      <button className={`coolbtn ${show ? "move-right" : ""}`}  onClick={toggleShow}>{button}</button>
+      <button id="me" className={`coolbtn ${show ? "move-right" : ""}`}  onClick={toggleShow}>{button}</button>
     </div>
   );
 }
