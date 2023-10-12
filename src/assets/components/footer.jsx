002e2b215dfd0296dd9a1aa019464a02e621a041
Jonte88 <jonathan.thj15@gmail.com>
2023-10-12 12:15:15 +0200

    Footer links 	modified:   src/assets/components/footer.jsx 	modified:   src/assets/css/footer.css

--- src/assets/components/footer.jsx
+++ src/assets/components/footer.jsx
@@ -1,24 +1,40 @@
 import "../css/footer.css";
-import Logo from '../img/logo.png';
+import Logo from "../img/logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
+
   return (
     <>
       <footer>
-        
-        <div>
-          <a href="#">Categories</a> | <a href="#">Something</a> |{" "}
-          <a href="#">Nothing</a> | <a href="#">Say Whatttt???</a>
+        <div className="footLinks">
+          <Link to={"/"}>
+            <div>Main Page</div>
+          </Link>
+          <div>&nbsp;|&nbsp;</div>
+          <Link to={"/games"}>
+            <div>Games</div>
+          </Link>
+          <div>&nbsp;|&nbsp;</div>
+          <Link to={"/profile"}>
+            <div>Profile</div>
+          </Link>
+          <div>&nbsp;|&nbsp;</div>
+          <Link to={"/about"}>
+            <div>About</div>
+          </Link>
         </div>
         <div className="logoDiv">
           <img src={Logo} alt="Logo" />
-          <h1>Game<span className="hubClass">hub</span></h1>
+          <h1>
+            Game<span className="hubClass">hub</span>
+          </h1>
         </div>
         <div>
           <p>Carl Krooks Gata 9, Helsingborg</p>
         </div>
         <div>
-            <span>Copyright &copy; 2023 Game Hub. No rights reserved.</span>
+          <span>Copyright &copy; 2023 Game Hub. No rights reserved.</span>
         </div>
       </footer>
     </>
