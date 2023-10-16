bd7f4708a9712d82f5d4f48e0b2bbb34f07f87bf
Mizfra <vipkotenok939@gmail.com>
2023-10-16 12:12:44 +0200

    text eddit about text

--- src/About.jsx
+++ src/About.jsx
@@ -131,53 +131,34 @@ function About() {
             <div className="infoGeneral">
               <h3 className="Textinfo">Game Hub</h3>
               <p className="AboutUs">
-                Game Hub är ett spännande projekt som sätter användarupplevelsen
-                i fokus. Här kan användare utforska och söka efter sina
-                favoritspel från Steam, samt enkelt lägga till dem som favoriter
-                för snabb och bekväm åtkomst. För att ta del av alla funktioner
-                erbjuder vi användarna möjligheten att logga in eller skapa ett
-                konto, vilket inte bara ger personlig åtkomst utan också
-                möjlighet att hålla koll på sina älsklingsspel.
+            Game Hub är ett spännande projekt som sätter användarupplevelsen i fokus. 
+            Här kan användare utforska och söka efter sina favoritspel från Steam samt enkelt lägga till dem som favoriter för snabb och bekväm åtkomst.
+            För att ta del av alla funktioner erbjuder vi användarna möjligheten att logga in eller skapa ett konto,
+            vilket inte bara ger personlig åtkomst utan också möjlighet att hålla koll 
+            på de spel man älskar mest.
               </p>
               <p className="AboutUs">
-                Upptäckning av nya spel är också en central del av Game Hub. Vår
-                huvudsida ger användare möjlighet att utforska och hitta nya
-                spel som passar deras intressen. Dessutom finns en slumpmässig
-                alternativfunktion som tar användaren till ett helt nytt och
-                spännande spel varje gång.
+              Uppteckningen av nya spel är också en central del av Game Hub. 
+              Vår huvudsida ger användare möjlighet att utforska och hitta nya spel som passar
+               deras intressen. Dessutom finns en slumpmässig alternativ funktion som tar
+                användaren till ett helt nytt och spännande spel varje gång. 
+                Vi jobbar även med att pusha upp spel som är mindre populära och ofta 
+                blir förbisedda. Bläddra bland 100 spel som inkluderar både de populäraste 
+                titlar samt de man aldrig har hört talas om innan. Vi hjälper dig att hitta 
+                nya upplevelser samt att spara dem för enklare åtkomst. 
+
               </p>{" "}
               <p className="AboutUs">
-                Vi använder en kraftfull API för att hämta information från
-                Steam, vilket ger våra användare tillgång till aktuell och
-                pålitlig data om deras favoritspel. För de som önskar köpa
-                spelen erbjuder vår sida även en smidig övergång till Steam för
-                köptransaktioner
+              Vi använder en kraftfull API för att hämta information om de 100 utvalda spel
+               från Steam, vilket ger våra användare tillgång till aktuell och pålitlig data
+                om deras favoritspel. För de som önskar köpa spelen erbjuder vår sida även en
+                 smidig övergång till Steam för vidare handel. 
+
               </p>
             </div>
           </div>
         </div>
     </div>
-
-
-    <div className="InfoGeneralCont">
-    
-    <div className="infoGeneral">
-        <h3 className="Textinfo">Game Hub</h3>
-        <p className="AboutUs"> 
-        Game Hub är ett spännande projekt som sätter användarupplevelsen i fokus. Här kan användare utforska och söka efter sina favoritspel från Steam samt enkelt lägga till dem som favoriter för snabb och bekväm åtkomst. För att ta del av alla funktioner erbjuder vi användarna möjligheten att logga in eller skapa ett konto, vilket inte bara ger personlig åtkomst utan också möjlighet att hålla koll på de spel man älskar mest.</p>
-<p className="AboutUs">
-Uppteckningen av nya spel är också en central del av Game Hub. Vår huvudsida ger användare möjlighet att utforska och hitta nya spel som passar deras intressen. Dessutom finns en slumpmässig alternativ funktion som tar användaren till ett helt nytt och spännande spel varje gång. Vi jobbar även med att pusha upp spel som är mindre populära och ofta blir förbisedda. Bläddra bland 100 spel som inkluderar både de populäraste titlar samt de man aldrig har hört talas om innan. Vi hjälper dig att hitta nya upplevelser samt att spara dem för enklare åtkomst. 
-
-</p> <p className="AboutUs">
-Vi använder en kraftfull API för att hämta information om de 100 utvalda spel från Steam, vilket ger våra användare tillgång till aktuell och pålitlig data om deras favoritspel. För de som önskar köpa spelen erbjuder vår sida även en smidig övergång till Steam för vidare handel. 
-</p>
-    </div>
-
-</div>
-
-</div>
-
-</div>
 
 
 
